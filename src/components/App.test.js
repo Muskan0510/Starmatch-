8272a0267abadafc6ad29b731b2e706c9d805a77
Muskan0510 <agrawal.muskan.1999@gmail.com>
2020-09-22 01:06:04 +0530

    test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,9 +7,6 @@ describe('App', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<App />).toJSON();
 
-    expect(tree).toMatchSnapshot({
-      createdAt: expect.any(Date),
-      id: expect.any(Number),
-    });
+    expect(tree).toMatchSnapshot();
   });
 });
